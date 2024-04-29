ba43d89eac00e15bea5f76064850dd8814501298
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-04-29 13:09:18 -0300

    Adicionando métodos para executar operações matemáticas e convertendo parâmetros de string para double.

--- src/main/java/br/com/erudio/MathController.java
+++ src/main/java/br/com/erudio/MathController.java
@@ -13,7 +13,25 @@ public class MathController {
 
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double greeting(@PathVariable(value = "numberOne") String numberOne,
-                             @PathVariable(value = "numberTwo") String numberTwo) {
-        return 1D;
+                             @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return converToDouble(numberOne) + converToDouble(numberTwo);
+    }
+
+    private Double converToDouble(String strNumber) {
+        if(strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",", ".");
+        if(isNumeric(strNumber)) {
+            return Double.parseDouble(number);
+        }
+        return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if(strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[+-]?[0-9]*\\.?[0-9]");
     }
 }
