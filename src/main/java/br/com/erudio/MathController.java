8e32ce75f29cdabe50c89d128ccbcd25888cb724
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-04-29 11:58:50 -0300

    Criando controller e trabalhando com a anotação @PathVariable.

--- src/main/java/br/com/erudio/MathController.java
+++ src/main/java/br/com/erudio/MathController.java
@@ -1,5 +1,6 @@
 package br.com.erudio;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-public class GreetingController {
-    private static final String template = "Hello, %s!";
+public class MathController {
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double greeting(@PathVariable(value = "numberOne") String numberOne,
+                             @PathVariable(value = "numberTwo") String numberTwo) {
+        return 1D;
     }
 }
