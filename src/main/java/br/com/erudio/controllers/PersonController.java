6b2f09db333140473c8ecce1b26ceee8912f9530
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-05-02 16:17:20 -0300

    Modificando controller, criando classe service e o método findById(), criando o model Person.java

---
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,23 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.model.Person;
+import br.com.erudio.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/person")
+@RestController
+public class PersonController {
+    @Autowired
+    private PersonService personService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET,
+    produces = MediaType.APPLICATION_PROBLEM_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") String id) {
+        return personService.findById(id);
+    }
+   }
---
+++ src/main/java/br/com/erudio/services/PersonService.java
@@ -0,0 +1,24 @@
+package br.com.erudio.services;
+
+import br.com.erudio.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonService {
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonService.class.getName());
+
+    public Person findById(String id) {
+        logger.info("Finding one person");
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Vinicius");
+        person.setLastName("Lemes");
+        person.setAddress("São Paulo - São Paulo - Brasil");
+        person.setGender("Male");
+        return person;
+    }
+}
