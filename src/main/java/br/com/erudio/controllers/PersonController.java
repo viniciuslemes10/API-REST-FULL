503d49cb378caf232575d39b6a04a7738932e727
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-05-03 14:48:03 -0300

    Criando CRUD.

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -4,10 +4,7 @@ import br.com.erudio.model.Person;
 import br.com.erudio.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +24,24 @@ public class PersonController {
     produces = MediaType.APPLICATION_PROBLEM_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") String id) {
         return personService.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+    produces = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+    consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return personService.create(person);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return personService.update(person);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "id") String id) {
+        personService.delete(id);
     }
    }
