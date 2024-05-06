3a23fd8e1ca74519bd3a1cd999e1761693858c4d
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-05-06 16:25:41 -0300

    Implementando Content Negotiation via QUERY Parameter.

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -17,31 +17,36 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public List<PersonVO> findAll() {
         return personService.findAll();
     }
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_PROBLEM_JSON_VALUE)
+    @GetMapping(value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public PersonVO findById(@PathVariable(value = "id") Long id) {
         return personService.findById(id);
     }
 
-    @PostMapping(produces = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
-    consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<PersonVO> create(@RequestBody PersonVO personVO) {
         PersonVO personVOCreated = personService.create(personVO);
         return ResponseEntity.status(HttpStatus.CREATED).body(personVOCreated);
     }
-    @PostMapping(value = "/v2", produces = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
-    consumes = MediaType.APPLICATION_JSON_VALUE)
+
+    @PostMapping(value = "/v2",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<PersonVOV2> createV2(@RequestBody PersonVOV2 personVO) {
         PersonVOV2 personVOCreated = personService.createV2(personVO);
         return ResponseEntity.status(HttpStatus.CREATED).body(personVOCreated);
     }
 
-    @PutMapping(produces = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public PersonVO update(@RequestBody PersonVO person) {
         return personService.update(person);
     }
