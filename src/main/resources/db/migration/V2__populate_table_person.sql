d25bb17700f5f6643a645b9fe5fe32721f78c4c6
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-05-06 15:28:47 -0300

    Adicionando flyway e migrations.

---
+++ src/main/resources/db/migration/V2__populate_table_person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` VALUES
+(1,'São Paulo - São Paulo - Brasil','Vinicius','Male','Lemes'),
+(3,'Bragança - São Paulo - Brasil','Lucas','Male','Souza'),
+(4,'São Paulo - São Paulo - Brasil','Leticia','Famele','Alcante'),
+(7,'São Caetano - São Paulo','Carlos','Male','Souza');
