7a7780765863aa9ca620a3e8b849647d920e8a19
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-05-10 14:44:59 -0300

    Adicionando dependência ModelMapper

--- src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
+++ src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
@@ -2,7 +2,6 @@ package br.com.erudio.data.vo.v1;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.github.dozermapper.core.Mapping;
 import org.springframework.hateoas.RepresentationModel;
 
 import java.io.Serializable;
@@ -13,7 +12,6 @@ public class PersonVO extends RepresentationModel<PersonVO> implements Serializa
     private static final long serialVersionUID = 1;
 
     @JsonProperty("id")
-    @Mapping("id")
     private Long key;
 
     @JsonProperty("first_name")
--- src/main/java/br/com/erudio/mapper/DozerMapper.java
+++ src/main/java/br/com/erudio/mapper/DozerMapper.java
@@ -1,14 +1,26 @@
 package br.com.erudio.mapper;
 
-import com.github.dozermapper.core.DozerBeanMapperBuilder;
-import com.github.dozermapper.core.Mapper;
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.model.Person;
+import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DozerMapper {
-    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
-//    private static ModelMapper mapper = new ModelMapper();
+//    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+    private static ModelMapper mapper = new ModelMapper();
+
+    static {
+        mapper.createTypeMap(
+                Person.class,
+                PersonVO.class)
+                .addMapping(Person::getId, PersonVO::setKey);
+        mapper.createTypeMap(
+                PersonVO.class,
+                Person.class
+        ).addMapping(PersonVO::getKey, Person::setId);
+    }
 
     public static <O, D> D parseObject(O origin, Class<D> destination) {
         return mapper.map(origin, destination);
