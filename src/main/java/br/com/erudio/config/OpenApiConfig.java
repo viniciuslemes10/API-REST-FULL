91b26081a3db36f58e02302574bd5968e3b82d48
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-05-15 16:59:54 -0300

    Adicionando dependência do spring-boot-starter-validation

--- src/main/java/br/com/erudio/config/OpenApiConfig.java
+++ src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
     @Bean
-    public OpenAPI customOpenAPI() {
+    OpenAPI customOpenAPI() {
        return new OpenAPI()
                 .info(
                         new Info()
