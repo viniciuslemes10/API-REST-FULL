6a6ced3c78ca3148faab757c922805877e68d8b4
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-06-20 17:15:56 -0300

    Implementando Token na documentação do Swagger

--- src/main/java/br/com/erudio/config/OpenApiConfig.java
+++ src/main/java/br/com/erudio/config/OpenApiConfig.java
@@ -4,26 +4,32 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
     @Bean
-    OpenAPI customOpenAPI() {
-       return new OpenAPI()
-                .info(
-                        new Info()
-                            .title("API RESTFull Java 17 and Spring Boot 3")
-                            .version("v1")
-                            .description("RESTFull API developed in-house and best practices")
-                            .license(
-                                    new License()
-                                        .url("http://pub.api.backend.com")
-                                        .name("Apache 2"))
-                                .termsOfService("http://pub.api.backend.com")
-                            .contact(new Contact()
-                                        .email("vinikjhgfds@gmail.com"))
-                );
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API RESTFull Java 17 and Spring Boot 3")
+                        .version("v1")
+                        .description("RESTFull API developed in-house and best practices")
+                        .license(new License()
+                                .url("http://pub.api.backend.com")
+                                .name("Apache 2"))
+                        .termsOfService("http://pub.api.backend.com")
+                        .contact(new Contact()
+                                .email("vinikjhgfds@gmail.com")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
