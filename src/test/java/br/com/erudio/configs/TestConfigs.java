d39ea7ec1083b77f91c3bbf111d72ee16cd362fe
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-07-25 16:59:38 -0300

    feat : retornando a porta

--- src/test/java/br/com/erudio/configs/TestConfigs.java
+++ src/test/java/br/com/erudio/configs/TestConfigs.java
@@ -1,7 +1,7 @@
 package br.com.erudio.configs;
 
 public class TestConfigs {
-    public static final int SERVER_PORT = 80;
+    public static final int SERVER_PORT = 8888;
     public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
     public static final String HEADER_PARAM_ORIGIN = "Origin";
     public static final String CONTENT_TYPE_JSON = "application/json";
